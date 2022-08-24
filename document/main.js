6167d61dc3948e9ebaed99952207571721d20beb
vairadurai <vairadurai007@gmail.com>
2022-08-24 12:01:55 +0530

    add

--- document/main.js
+++ document/main.js
@@ -1,9 +1,21 @@
 
 
 
-       
 
-       function clickme(){
-        let a=document.getElementById("click").innerHTML="amukku dumukku amal dumal";
-        alert(a);
-                }
+
+function clickme() {
+    let a = document.getElementById("click").innerHTML = "amukku dumukku amal dumal";
+    alert(a);
+}
+
+
+function confirm() {
+    let x = document.getElementById("pass").value;
+    let y = document.getElementById("con").value;
+    if (x == y) {
+        document.getElementById("yes").innerHTML = "currect";
+    }
+    else {
+        document.getElementById("yes").innerHTML = "in currect";
+    }
+}
