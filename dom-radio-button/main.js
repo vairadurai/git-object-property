d1ad3fe917b486e3fcea73efab76d00d284b4157
vairadurai <vairadurai007@gmail.com>
2022-08-28 19:15:39 +0530

    add

---
+++ dom-radio-button/main.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+// DOM Ratio button
+
+function durai() {
+	let a=document.getElementById("html");
+	let b=document.getElementById("css");
+	let c=document.getElementById("java");
+	if (a.checked==true)
+		alert("U have selected "+a.value);
+	else if(b.checked==true)
+		alert("u have selected"+b.value);
+	else if(c.checked==true)
+		alert("u have selected"+c.value);
+		
+}
--- dom-table-concept/main.js
+++ dom-table-concept/main.js
@@ -3,18 +3,99 @@
 
 
 
-function calculate(){
-    let unit_price=document.getElementById("unitprice").innerText,
-    quantity=document.getElementById("qty").value,
-    action=document.getElementById("total");
+// function calculate(){
+//     let unit_price=document.getElementById("unitprice").innerText,
+//     quantity=document.getElementById("qty").value,
+//     action=document.getElementById("total");
     
 
-    price=unit_price*quantity;
-    document.getElementById("price").innerHTML=price;
-    // return total;
+//     price=unit_price*quantity;
+//     document.getElementById("price").innerHTML=price;
+//     // return total;
 
+// }
+
+// document.getElementById ("total").addEventListener('click',calculate);
+
+
+// function cal() {
+//     let unitElement=document.getElementsByClassName("unit").innerText;
+//     let quanElement=document.getElementsByClassName("quan").value;
+//     let  actElement=document.getElementsByClassName("act");
+
+//     prize=unitElement*quanElement;
+//     document.getElementsByClassName("prize").innerHTML=prize;
+// }
+// let a=document.getElementsByClassName("act");
+// console.log(a);
+// // document.getElementsByClassName("act").addEventListener('click',cal);
+
+
+
+
+function calculation(){
+    let rate,qty,cost,
+    select=this.parentNode.parentNode,
+   un_p=select.getElementsByClassName("unitprice")[0],
+    qtyele=select.getElementsByClassName("quantity")[0],
+    total=select.getElementsByClassName("price")[0];
+    rate=un_p.innerText;
+    qty=qtyele.value;
+    rate=parseInt(rate);
+    cost=(rate*qty);
+    
+    total.innerHTML=cost;
+    // total=parseInt(cost);
+}
+let x=document.getElementsByClassName("action");
+// console.log(x);
+
+for(i=0;i<x.length;i++){
+    x[i].addEventListener("click",calculation);
+    console.log(x[i]);
 }
 
-document.getElementById ("total").addEventListener('click',calculate);
-// let a= calculate();
-// console.log(a);
+// function calculation(){
+
+//     let select=this.parentNode.parentNode,
+//    un_p=select.getElementsByClassName("unitprice")[0],
+//     qty=select.getElementsByClassName("quantity")[0].Value,
+//     cost,
+//     total=select.getElementsByClassName("price")[0];
+//     un_p=un_p.innerText;
+//     un_p=parseInt(un_p);
+//     cost=un_p*qty;
+    
+//     total.innerHTML=cost;
+//     total=parseInt(cost);
+// }
+// let x=document.getElementsByClassName("action");
+// console.log(x);
+
+// for(i=0;i<x.length;i++){
+//     x[i].addEventListener("click",calculation);
+//     console.log(x[i]);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
